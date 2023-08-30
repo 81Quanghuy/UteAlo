ac40347bf914ac7b8e2d13bf84c9cb340b383d2f
81QuangHuy <ngoquanghuy2210@gmail.com>
2023-08-30 09:33:22 +0700

    update repo,service

--- BackEnd/UTEALO/src/main/java/vn/iostar/entity/ChatGroupMember.java
+++ BackEnd/UTEALO/src/main/java/vn/iostar/entity/ChatGroupMember.java
@@ -1,4 +1,4 @@
-package vn.iostar.utealo;
+package vn.iostar.entity;
 
 import java.io.Serializable;
 import java.util.List;
--- BackEnd/UTEALO/src/main/java/vn/iostar/entity/User.java
+++ BackEnd/UTEALO/src/main/java/vn/iostar/entity/User.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import vn.iostar.entity.ChatGroupMember;
 
 @Getter
 @Setter
