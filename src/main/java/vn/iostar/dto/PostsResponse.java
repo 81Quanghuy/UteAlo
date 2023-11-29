4176fa434e932ea69ff97e256a9862a6b3910c34
81QuangHuy <20110650@student.hcmute.edu.vn>
2023-11-29 10:24:01 +0700

    update feed Timeline

--- src/main/java/vn/iostar/dto/PostsResponse.java
+++ src/main/java/vn/iostar/dto/PostsResponse.java
@@ -8,55 +8,57 @@ import vn.iostar.contants.PrivacyLevel;
 import vn.iostar.contants.RoleName;
 import vn.iostar.entity.Post;
 
-
 @Data
 public class PostsResponse {
 	private int postId;
 	private Date postTime;
-    private Date updateAt;
-    private String content;
-    private String photos;
-    private String files;
-    private String location;
-    private String userId;
-    private String userName;
-    private int postGroupId;
-    private String postGroupName;
-    private List<Integer> comments;
-    private List<Integer> likes;
-    private RoleName roleName;
-    private PrivacyLevel privacyLevel;
-    
-    
+	private Date updateAt;
+	private String content;
+	private String photos;
+	private String files;
+	private String location;
+	private String userId;
+	private String userName;
+	private String avatarUser;
+	private int postGroupId;
+	private String postGroupName;
+	private String avatarGroup;
+	private List<Integer> comments;
+	private List<Integer> likes;
+	private RoleName roleName;
+	private PrivacyLevel privacyLevel;
+
 	public PostsResponse(Post post) {
 		this.postId = post.getPostId();
 		this.postTime = post.getPostTime();
 		this.updateAt = post.getUpdateAt();
-		if(post.getContent()!=null) {
+		if (post.getContent() != null) {
 			this.content = post.getContent();
 		}
-		if(post.getPhotos()!=null) {
+		if (post.getPhotos() != null) {
 			this.photos = post.getPhotos();
 		}
-		if(post.getFiles()!=null) {
+		if (post.getFiles() != null) {
 			this.files = post.getFiles();
 		}
-		if(post.getLocation()!=null) {
+		if (post.getLocation() != null) {
 			this.location = post.getLocation();
 		}
 		this.userId = post.getUser().getUserId();
-		if(post.getUser().getUserName() != null) {
+		this.avatarUser = post.getUser().getProfile().getAvatar();
+		if (post.getUser().getUserName() != null) {
 			this.userName = post.getUser().getUserName();
 		}
-		if(post.getPostGroup() != null) {
+		if (post.getPostGroup() != null) {
 			this.postGroupId = post.getPostGroup().getPostGroupId();
 			this.postGroupName = post.getPostGroup().getPostGroupName();
+			this.avatarGroup = post.getPostGroup().getAvatarGroup();
 		}
-		if(post.getUser().getRole().getRoleName()!=null) {
+		if (post.getUser().getRole().getRoleName() != null) {
 			this.roleName = post.getUser().getRole().getRoleName();
 		}
-		if(post.getPrivacyLevel()!=null) {
-			this.privacyLevel = post.getPrivacyLevel(); 
+		if (post.getPrivacyLevel() != null) {
+			this.privacyLevel = post.getPrivacyLevel();
 		}
 	}
 }
--- src/main/java/vn/iostar/dto/SharesResponse.java
+++ src/main/java/vn/iostar/dto/SharesResponse.java
@@ -4,8 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
+import vn.iostar.contants.PrivacyLevel;
+import vn.iostar.contants.RoleName;
 import vn.iostar.entity.Share;
-
 
 @Data
 public class SharesResponse {
@@ -14,18 +15,32 @@ public class SharesResponse {
 	private String content;
 	private Date createAt;
 	private Date updateAt;
-	private int postId;
+	private RoleName roleName;
 	private String userId;
+	private String userName;
+	private String avatarUser;
+	private PrivacyLevel privacyLevel;
+	private PostsResponse postsResponse;
+	private Integer postGroupId;
+	private String postGroupName;
 	private List<Integer> comments;
 	private List<Integer> likes;
-	
+
 	public SharesResponse(Share share) {
 		this.shareId = share.getShareId();
 		this.content = share.getContent();
 		this.createAt = share.getCreateAt();
 		this.updateAt = share.getUpdateAt();
-		this.postId = share.getPost().getPostId();
+		this.postsResponse = new PostsResponse(share.getPost());
 		this.userId = share.getUser().getUserId();
+		this.avatarUser = share.getUser().getProfile().getAvatar();
+		this.roleName = share.getUser().getRole().getRoleName();
+		this.userName = share.getUser().getUserName();
+		this.privacyLevel = share.getPrivacyLevel();
+		if (share.getPostGroup() != null) {
+			this.postGroupId = share.getPostGroup().getPostGroupId();
+			this.postGroupName = share.getPostGroup().getPostGroupName();
+		}
 
 	}
 }
